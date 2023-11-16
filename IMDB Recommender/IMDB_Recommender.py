4ae2855ee68aa5e85bdbe3f12236f0f3ff187707
İbrahim Soner İNAN <isonerinan@gmail.com>
2023-11-16 22:46:40 +0300

    Version Change

--- IMDB Recommender/IMDB_Recommender.py
+++ IMDB Recommender/IMDB_Recommender.py
@@ -3724,7 +3724,7 @@ class ModernApp(QMainWindow):
         msg.setIcon(QMessageBox.Information)
         msg.setWindowTitle("About")
         msg.setText("<h1>IMDB Recommender</h1>"
-                    "<h3>Version 3.6.5</h3>"
+                    "<h3>Version 3.8</h3>"
                     "<b>Created by:</b> İbrahim Soner İNAN<br><br>"
                     "<a href='https://github.com/isonerinan'>GitHub</a><br><br>"
                     "<a href='https://www.linkedin.com/in/isonerinan'>LinkedIn</a><br><br>"
